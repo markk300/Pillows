0f427f89c0cf8e27f78f3359a68291bd91efe08f
iammark <mrdamarkomm@gmail.com>
2024-02-06 22:51:32 +0100

    added responsive to HappySleepers

--- src/components/Home/HappySleepers.js
+++ src/components/Home/HappySleepers.js
@@ -21,15 +21,15 @@ export const HappySleepers = () => {
           With over <b className='font-bold'>20,000 five-star reviews</b>, don’t take our word for it. Take theirs.
           </h2>
         </div>
-        <div className='flex flex-row mt-20 text-center justify-center  '>
+        <div className='flex flex-col items-center md:flex md:flex-row mt-5 md:mt-20 text-center md:justify-center  '>
             {reviewData.map((data) =>(
                 <div key={data.id} className='w-[390px]'>
-                    <div className='pb-8'>
+                    <div className='pb-3 md:pb-8'>
                     <StarsComponent stars={data.stars}/>
                     </div>
                     <h1 className='text-primary font-bold text-[22px] pb-2'>{data.title}</h1>
                     <p className='text-[12px] pb-3'>{data.desc}</p>
-                    <h2 className='text-[#406C93] italic'>{data.author}</h2>
+                    <h2 className='text-[#406C93] italic mb-3'>{data.author}</h2>
                 </div>
             ))}
         </div>
